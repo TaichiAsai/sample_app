a08e90df62fb3c49d0b2f94a42d3b39c1dde1ffc
TaichiAsai <t-asai@tss-ipnet.co.jp>
2017-04-18 10:10:57 +0900

    Finish log in/log out

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,11 +5,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 1
+    assert_select "a[href=?]", root_path, count:2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
-    get signup_path
-    assert_select "title", full_title("Sign up")
+    #get signup_path
+    #assert_select "title", full_title("Sign up")
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -57,19 +57,23 @@ class UserTest < ActiveSupport::TestCase
   end
   
   test "email addresses should be saved as lower-case" do
-  	mixed_case_email = "Foo@ExAMPle.CoM"
-  	@user.email = mixed_case_email
-  	@user.save
-  	assert_equal mixed_case_email.downcase, @user.reload.email
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
   
   test "password should be present (nonblank)" do
-  	@user.password = @user.password_confirmation = " " * 6
-  	assert_not @user.valid?
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
   
-  test "password should have a minimum length" do
-  	@user.password = @user.password_confirmation = "a" * 5
-  	assert_not @user.valid?
+  test "authenticated? should return false for a user with nil digest" do
+    assert_not @user.authenticated?('')
   end
 end
