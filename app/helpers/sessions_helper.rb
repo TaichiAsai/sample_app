b4ed29e6bf9932ae42359c03c78d405e559c715d
TaichiAsai <t-asai@tss-ipnet.co.jp>
2017-04-19 11:32:52 +0900

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,21 @@
 module SessionsHelper
 
-	# 渡されたユーザーでログインする
-	def log_in(user)
-		session[:user_id] = user.id
-	end
-	
-	# ユーザーを永続的にセッションに記憶する
-	def remember(user)
-		user.remember
+	# 与えられたユーザーをログイン
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 永続セッションとしてユーザーを記憶する
+  def remember(user)
+    user.remember
     cookies.permanent.signed[:user_id] = user.id
     cookies.permanent[:remember_token] = user.remember_token
-	end
+  end
+  
+  # 与えられたユーザーがログイン済みユーザーであればtrueを返す
+  def current_user?(user)
+  	user == current_user
+  end
 	
 	# 現在ログインしているユーザーを返す（ユーザーがログイン中の場合のみ）
 	def current_user
@@ -47,5 +52,16 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  # 記憶したURL (もしくはデフォルト値) にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
